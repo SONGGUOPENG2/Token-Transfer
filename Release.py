e4e20806e052d83c317520857a54c0f2c6572cde
SONGGUOPENG <sandhooking@outlook.com>
2025-06-12 17:46:17 +0800

    Add files via upload

---
+++ Release.py
@@ -0,0 +1,14 @@
+"""
+软件名称：SONGGUOPENG系列软件
+最早发行版本：2024.05.13
+最后发行版本：2025.06.10
+发行地址：https://drive.google.com/drive/folders/1-04YEI42YL5oc5Z5mLs0IQV6yeOow8m2?usp=drive_link
+托管地址：https://github.com/SONGGUOPENG2?tab=repositories
+Copyright ©2025 SONGGUOPENG All Rights Reserved
+
+请在Microsoft Skype(Teams)与我对话或通话：
+https://join.skype.com/invite/vMnpR1gq7giA
+https://teams.live.com/l/invite/FEAxomFaPlCD5CY_QM
+请在Telegram与我对话或通话：
+https://t.me/SONGGUOPENG
+"""
